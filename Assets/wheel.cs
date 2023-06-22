1b1213dd31041f849c065e8b084baa384e76e33d
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-06-22 18:47:45 -0300

    Car functionality

--- Assets/wheel.cs
+++ Assets/wheel.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class wheel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] WheelCollider frontRight;
+    [SerializeField] WheelCollider frontLeft;
+    [SerializeField] WheelCollider backRight;
+    [SerializeField] WheelCollider backLeft;
+
+    [SerializeField] Transform frontRightWheel;
+    [SerializeField] Transform frontLeftWheel;
+    [SerializeField] Transform backRightWheel;
+    [SerializeField] Transform backLeftWheel;
+
+    public float acceleration = 500f;
+    public float breakingForce = 300f;
+    public float maxTurnAngle = 15f;
+
+    private float currentAcceleration = 0f;
+    private float currentBreakForce = 0f;
+    private float currentTurnAngle = 0f;
+
+    private void FixedUpdate()
     {
-        
+        currentAcceleration = acceleration * Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentBreakForce = breakingForce;
+        }
+        else
+        {
+            currentBreakForce = 0f;
+        }
+
+        frontRight.motorTorque = currentAcceleration;
+        frontLeft.motorTorque = currentAcceleration;
+
+        frontRight.brakeTorque = currentBreakForce;
+        frontLeft.brakeTorque = currentBreakForce;
+        backRight.brakeTorque = currentBreakForce;
+        backLeft.brakeTorque = currentBreakForce;
+
+        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+        frontLeft.steerAngle = currentTurnAngle;
+        frontRight.steerAngle = currentTurnAngle;
+
+        UpdateWheel(frontLeft, frontLeftWheel);
+        UpdateWheel(frontRight, frontRightWheel);
+        UpdateWheel(backLeft, backLeftWheel);
+        UpdateWheel(backRight, backRightWheel);
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateWheel(WheelCollider col, Transform trans)
     {
-        
+        Vector3 position;
+        Quaternion rotation;
+        col.GetWorldPose(out position, out rotation);
+
+        trans.position = position;
+        trans.rotation = rotation;
     }
 }
